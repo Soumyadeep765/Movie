2c920b6809935eab10a26fa64427f03ac5c3f1d3
Soumyadeep765 <133207548+Soumyadeep765@users.noreply.github.com>
2023-12-25 11:39:53 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "paste your vnshortner api key here"
+api_key = "2982bf9774b94d3d9f20f822a00204d876ce3a57"
 
 
 def search_movies(query):
@@ -32,7 +32,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://vnshortener.com/api?api={api_key}&url={i['href']}"
+            url = f"https://tnshort.net/st?api={api_key}&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
